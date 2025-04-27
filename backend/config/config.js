0336eae08f705295805e0b30a38ab2b7af0de75c
abhijit732023 <abhijitgautam2004@gmail.com>
2025-04-27 22:20:34 +0530

    made1

--- backend/config/config.js
+++ backend/config/config.js
@@ -4,5 +4,7 @@ const config = {
     mongoURL: process.env.VITE_MONGO_URL,
     screteCode: process.env.VITE_SECRET_CODE,
     backendURl: process.env.VITE_BACKEND_URL,
+    payment_key_id: process.env.VITE_KEY_ID,
+    payment_key_secret: process.env.VITE_KEY_SECRET,
 };
 export default config;
